57caca0eea9f3d0992f2b20563155070c21fc99c
WBot3000 <wvenusaur@gmail.com>
2022-12-10 12:39:47 -0500

    Added error checks and lots of comments

--- MRTD.py
+++ MRTD.py
@@ -110,6 +110,11 @@ def calculateCheck(lst):
         total += (numVal * weights[weightIdx])
         weightIdx = (weightIdx + 1) % len(weights)
     return total % 10
+
+def validateDate(date):
+    for char in date:
+        if(char != "<" and (char < "0" or char > "9")):
+            raise Exception("Invalid character " + char + " in date")
     
 
 #Requirement 1
@@ -121,7 +126,6 @@ def scanMRZ():
 #Requirement 2
 #Each line seems to be 44 characters
 def decodeMRZ():
-    #TODO
     (line1, line2) = scanMRZ()
     if(len(line1) != LINE_LENGTH or len(line2) != LINE_LENGTH):
         raise Exception("Lines must both be " + str(LINE_LENGTH) + " characters.")
@@ -134,18 +138,24 @@ def decodeMRZ():
     travelData.issuingCountry = line1[linePos:linePos+3]
     linePos +=3
     while(line1[linePos] != "<"): #Parsing last name
+        if(line1[linePos] < "A" or line1[linePos] > "Z"): #Check to make sure the name is valid
+            raise Exception("Invalid character " + line1[linePos] + " in last name")
         currentString += line1[linePos]
         linePos += 1
     travelData.lastName = currentString
     linePos += 2 #Skip the second < and go straight to the first letter of the first name
     currentString = "" #So you can use it again for the other fields
     while(line1[linePos] != "<"):
+        if(line1[linePos] < "A" or line1[linePos] > "Z"):
+            raise Exception("Invalid character " + line1[linePos] + " in first name")
         currentString += line1[linePos]
         linePos += 1
     travelData.firstName = currentString
     linePos += 1
     currentString = ""
     while(line1[linePos] != "<"):
+        if(line1[linePos] < "A" or line1[linePos] > "Z"):
+            raise Exception("Invalid character " + line1[linePos] + " in middle name")
         currentString += line1[linePos]
         linePos += 1
     travelData.middleName = currentString
@@ -161,12 +171,14 @@ def decodeMRZ():
     travelData.countryCode = line2[linePos:linePos+COUNTRY_CODE_LENGTH]
     linePos += COUNTRY_CODE_LENGTH
     travelData.birthday = line2[linePos:linePos+DATE_LENGTH]
+    validateDate(travelData.birthday)
     linePos += DATE_LENGTH
     travelData.birthdayCheck = line2[linePos:linePos+1]
     linePos += 1
     travelData.sex = line2[linePos:linePos+SEX_LENGTH]
     linePos += 1
     travelData.expirationDate = line2[linePos:linePos+DATE_LENGTH]
+    validateDate(travelData.expirationDate)
     linePos += DATE_LENGTH
     travelData.expirationCheck = line2[linePos:linePos+1]
     linePos += 1
@@ -187,12 +199,16 @@ def encodeMRZ(personalNo):
     data = getTravelDataFromDB(personalNo) #What needs to be mocked in test cases, this implementation expects a TravelData object
     #Encoding Line 1
     line1 = data.docType + "<" + data.issuingCountry + data.lastName + "<<" + data.firstName + "<" + data.middleName
+    if (len(line1) > LINE_LENGTH):
+        raise Exception("First line is not " + str(LINE_LENGTH) + " characters.")
     while(len(line1) < LINE_LENGTH):
         line1 += "<"
 
     #Encoding Line 2
     line2 = data.passportNo + data.passportCheck + data.countryCode + data.birthday + data.birthdayCheck + data.sex + data.expirationDate + data.expirationCheck + data.personalNo + data.personalNoCheck
     #NOTE: Padding is stored in the personal number according to decodeMRZ, so none added here
+    if (len(line2) != LINE_LENGTH):
+        raise Exception("Second line is not " + str(LINE_LENGTH) + " characters.")
     return (line1, line2)
 
 
--- MRTDtest.py
+++ MRTDtest.py
@@ -10,25 +10,45 @@ testData3 = ['7','4','0','8','1','2']
 testData4 = ['1','2','0','4','1','5']
 testData5 = ['Z','E','1','8','4','2','2','6','B']
 
-#For checking other functions
+#For checking all the other functions
+#Standard user MRZ lines
 annaLine1 = "P<UTOERIKSSON<<ANNA<MARIA<<<<<<<<<<<<<<<<<<<"
 annaLine2 = "L898902C36UTO7408122F1204159ZE184226B<<<<<<1"
 georgeLine1 = "P<UTOJORGE<<GEORGE<G<<<<<<<<<<<<<<<<<<<<<<<<"
 georgeLine2 = "G30RG3ABC6UTO6909264M1405116EGROEG<<<<<<<<<7"
 willLine1 = "P<UTOFRANKLIN<<WILLIAM<TRAVIS<<<<<<<<<<<<<<<"
 willLine2 = "46NQW09071UTO9201302M1603039WX123U22XIESAL<9"
+
+#Used to check edge cases for decoding MRZ. Besides too long/too short, they are based off of Anna's lines
 tooLongLine = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
 tooShortLine = "B"
+invalidLastNameLine = "P<UTOERIKSS0N<<ANNA<MARIA<<<<<<<<<<<<<<<<<<<"
+invalidFirstNameLine = "P<UTOERIKSSON<<ANN@<MARIA<<<<<<<<<<<<<<<<<<<"
+invalidMiddleNameLine = "P<UTOERIKSSON<<ANNA<MAR1A<<<<<<<<<<<<<<<<<<<"
+invalidBirthdayLine = "L898902C36UTO7N08122F1204159ZE184226B<<<<<<1"
+partialBirthdayLine = "L898902C36UTO74<<122F1204159ZE184226B<<<<<<1"
+invalidExpirationLine = "L898902C36UTO7408122F1204L59ZE184226B<<<<<<1"
+partialExpirationLine = "L898902C36UTO7408122F<<04159ZE184226B<<<<<<1"
 
+#Three users
 annaData = TravelData("P", "UTO", "ERIKSSON", "ANNA", "MARIA", "L898902C3", "6", "UTO", "740812", "2", "F", "120415", "9", "ZE184226B<<<<<<", "1")
 georgeData = TravelData("P", "UTO", "JORGE", "GEORGE", "G", "G30RG3ABC", "6", "UTO", "690926", "4", "M", "140511", "6", "EGROEG<<<<<<<<<", "7")
 willData = TravelData("P", "UTO", "FRANKLIN", "WILLIAM", "TRAVIS", "46NQW0907", "1", "UTO", "920130", "2", "M", "160303", "9", "WX123U22XIESAL<", "9" )
+
+#Used to make sure partial dates are accepted
+partialBDayData = TravelData("P", "UTO", "ERIKSSON", "ANNA", "MARIA", "L898902C3", "6", "UTO", "74<<12", "2", "F", "120415", "9", "ZE184226B<<<<<<", "1")
+partialExpirationData = TravelData("P", "UTO", "ERIKSSON", "ANNA", "MARIA", "L898902C3", "6", "UTO", "740812", "2", "F", "<<0415", "9", "ZE184226B<<<<<<", "1")
+
+#Checks to make sure decoded data isn't too much
+tooLongData = TravelData("P", "UTO", "ERIKSSON", "ANNA", tooLongLine, "L898902C3", "6", "UTO", "740812", "2", "F", "120415", "9", "TESTINGLONG<<<<", "1")
+#Should throw b/c it's second line, would be padded otherwise
+tooShortData = TravelData("P", "UTO", "ERIKSSON", "ANNA", "MARIA", "L898902C3", "6", "UTO", "740812", "2", "F", "120415", "9", tooShortLine, "1")
 
 annaCheck = TravelDataError(False, False, False, False)
 georgeCheck = TravelDataError(False, False, True, True)
 willCheck = TravelDataError(True, True, False, False)
 
-#Used to check if two objects have all the same field values or not
+#Used to check if two objects have all the same field values or not, helpful for testing object equality
 def checkDataEquality(d1, d2):
     d1Fields = vars(d1)
     d2Fields = vars(d2)
@@ -38,6 +58,7 @@ def checkDataEquality(d1, d2):
             return False
     return True
 
+#Since scanMRZ doesn't take in any input arguments, multiple functions mock it based on what card it would be scanning
 def scanAnna():
     return (annaLine1, annaLine2)
 
@@ -52,7 +73,30 @@ def scanTooMuch():
 
 def scanTooLittle():
     return (tooShortLine, annaLine2)
-    
+
+def scanInvalidLast():
+    return (invalidLastNameLine, annaLine2)
+
+def scanInvalidFirst():
+    return (invalidFirstNameLine, annaLine2)
+
+def scanInvalidMiddle():
+    return (invalidMiddleNameLine, annaLine2)
+
+def scanInvalidBirthday():
+    return (annaLine1, invalidBirthdayLine)
+
+def scanPartialBirthday():
+    return (annaLine1, partialBirthdayLine)
+
+def scanInvalidExpiration():
+    return (annaLine1, invalidExpirationLine)
+
+def scanPartialExpiration():
+    return (annaLine1, partialExpirationLine)
+
+
+#Replicates the function of database retrieval, assumes that the personal number is unique for each user
 def mockDBFunc(personalNo):
     if(personalNo == "ZE184226B<<<<<<"):
         return annaData
@@ -60,16 +104,22 @@ def mockDBFunc(personalNo):
         return georgeData
     if(personalNo == "WX123U22XIESAL<"):
         return willData
+    if(personalNo == "TESTINGLONG<<<<"):
+        return tooLongData
+    if(personalNo == tooShortLine):
+        return tooShortData
     
 
 class TestMRTD(unittest.TestCase):
-    # define multiple sets of tests as functions with names that begin
+    #Define multiple sets of tests as functions with names that begin
 
+    #Make sure non-numerical characters are returning the right numerical values
     def testGetNumericalValue(self):
         self.assertEqual(getNumericalValue("A"), 10, "A should be 10")
         self.assertEqual(getNumericalValue("Z"), 35, "Z should be 35")
         self.assertEqual(getNumericalValue("<"), 0, "< should be 0")
-        
+
+    #Make sure check digit calculator is calculating the correct check digits
     def testCalculateCheck(self):
         self.assertEqual(calculateCheck(testData), 5, "Should be 5")
         self.assertEqual(calculateCheck(testData2), 6, "Should be 6")
@@ -77,35 +127,87 @@ class TestMRTD(unittest.TestCase):
         self.assertEqual(calculateCheck(testData4), 9, "Should be 9")
         self.assertEqual(calculateCheck(testData5), 1, "Should be 1")
 
+    #Test cases for decodeMRZ. Check to see that lines can be converted into data fields properly. Need multiple since a different mock is used for each scenario
+    #Anna
     @mock.patch("MRTD.scanMRZ", side_effect = scanAnna)
     def testDecodeMRZ(self, mockScan):
         self.assertTrue(checkDataEquality(decodeMRZ(), annaData), "Data for Anna not being decoded correctly")
 
+    #George
     @mock.patch("MRTD.scanMRZ", side_effect = scanGeorge)
     def testDecodeMRZ2(self, mockScan):
         self.assertTrue(checkDataEquality(decodeMRZ(), georgeData), "Data for George not being decoded correctly")
 
+    #Will
     @mock.patch("MRTD.scanMRZ", side_effect = scanWill)
     def testDecodeMRZ3(self, mockScan):
         self.assertTrue(checkDataEquality(decodeMRZ(), willData), "Data for William not being decoded correctly")
 
+    #Line too long
     @mock.patch("MRTD.scanMRZ", side_effect = scanTooMuch)
     def testDecodeMRZ4(self, mockScan):
         with self.assertRaises(Exception):
             decodeMRZ()
 
+    #Line too short
     @mock.patch("MRTD.scanMRZ", side_effect = scanTooLittle)
     def testDecodeMRZ5(self, mockScan):
         with self.assertRaises(Exception):
             decodeMRZ()
 
+    #Last name has invalid characters
+    @mock.patch("MRTD.scanMRZ", side_effect = scanInvalidLast)
+    def testDecodeMRZ6(self, mockScan):
+        with self.assertRaises(Exception):
+            decodeMRZ()
+
+    #First name has invalid characters
+    @mock.patch("MRTD.scanMRZ", side_effect = scanInvalidFirst)
+    def testDecodeMRZ7(self, mockScan):
+        with self.assertRaises(Exception):
+            decodeMRZ()
+
+    #Middle name has invalid characters
+    @mock.patch("MRTD.scanMRZ", side_effect = scanInvalidMiddle)
+    def testDecodeMRZ8(self, mockScan):
+        with self.assertRaises(Exception):
+            decodeMRZ()
+
+    #Birthday has invalid characters
+    @mock.patch("MRTD.scanMRZ", side_effect = scanInvalidBirthday)
+    def testDecodeMRZ9(self, mockScan):
+        with self.assertRaises(Exception):
+            decodeMRZ()
+
+    #Birthday has placeholder characters
+    @mock.patch("MRTD.scanMRZ", side_effect = scanPartialBirthday)
+    def testDecodeMRZ10(self, mockScan):
+        self.assertTrue(checkDataEquality(decodeMRZ(), partialBDayData), "Data for partial birthday user not being decoded correctly")
+
+    #Expiration Date has invalid characters
+    @mock.patch("MRTD.scanMRZ", side_effect = scanInvalidExpiration)
+    def testDecodeMRZ11(self, mockScan):
+        with self.assertRaises(Exception):
+            decodeMRZ()
+
+    #Expiration Date has partial characters
+    @mock.patch("MRTD.scanMRZ", side_effect = scanPartialExpiration)
+    def testDecodeMRZ12(self, mockScan):
+        self.assertTrue(checkDataEquality(decodeMRZ(), partialExpirationData), "Data for partial expiration date user not being decoded correctly")
+
+
+    #Checks to see if data from database is being converted into appropriate MRZ lines
     @mock.patch("MRTD.getTravelDataFromDB", side_effect = mockDBFunc)
     def testEncodeMRZ(self, mockData):
         self.assertEqual(encodeMRZ("ZE184226B<<<<<<"), (annaLine1, annaLine2), "Data for Anna not being encoded properly")
         self.assertEqual(encodeMRZ("EGROEG<<<<<<<<<"), (georgeLine1, georgeLine2), "Data for George not being encoded properly")
         self.assertEqual(encodeMRZ("WX123U22XIESAL<"), (willLine1, willLine2), "Data for William not being encoded properly")
+        with self.assertRaises(Exception):
+            encodeMRZ("TESTINGLONG<<<<")
+        with self.assertRaises(Exception):
+            encodeMRZ(tooShortLine)
 
-
+    #Checks to see if mismatch data is being calculated correctly
     def testCheckMismatches(self):
         self.assertTrue(checkDataEquality(checkMismatches(annaData), annaCheck), "Improper errors calculated for Anna")
         self.assertTrue(checkDataEquality(checkMismatches(georgeData), georgeCheck), "Improper errors calculated for George")
